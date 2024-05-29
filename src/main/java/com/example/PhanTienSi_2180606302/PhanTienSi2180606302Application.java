8f28e0cd885e6ae950d4a16e52b92b7d27562436
phantiensi113 <phantiensi113@gmail.com>
2024-05-29 22:52:25 +0700

    tôi là phan tiến sĩ

---
+++ src/main/java/com/example/PhanTienSi_2180606302/PhanTienSi2180606302Application.java
@@ -0,0 +1,13 @@
+package com.example.PhanTienSi_2180606302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhanTienSi2180606302Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhanTienSi2180606302Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PhanTienSi_2180606302/PhanTienSi2180606302ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PhanTienSi_2180606302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhanTienSi2180606302ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
